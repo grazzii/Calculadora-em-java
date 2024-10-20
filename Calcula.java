6ecc7838417a16d3cff88de881dca998d437059b
Grazi <124455538+grazzii@users.noreply.github.com>
2024-10-20 18:17:18 +0000

    second

---
+++ Calcula.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Calcula {
+
+    // Método que faz a multiplicação de dois números
+    public int multiplicar(int numero1, int numero2) {
+        return numero1 * numero2;
+    }
+}
--- Nome.java
+++ Nome.java
@@ -1,15 +1,32 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class Nome {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    
+
         System.out.println("Oi usuário!");
         System.out.println("Digite seu nome: ");
-
         String nome = sc.nextLine();
-        System.out.println("Seja bem vindo! " + nome);
-
         
+        System.out.println("Seja bem-vindo! " + nome);
+        System.out.println("Agora, faça a multiplicação de dois números:");
+
+        // Solicita dois números ao usuário
+        System.out.print("Escolha um número: ");
+        int numero1 = sc.nextInt();
+        
+        System.out.print("Escolha um segundo número: ");
+        int numero2 = sc.nextInt();
+
+        // Cria uma instância da classe Calcula
+        Calcula calcula = new Calcula();
+        
+        // Chama o método multiplicar da classe Calcula
+        int resultado = calcula.multiplicar(numero1, numero2);
+        
+        // Exibe o resultado
+        System.out.println("O resultado da multiplicação é: " + resultado);
+        
+        sc.close();
     }
 }
